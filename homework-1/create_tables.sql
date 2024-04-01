3e769c70e618418d53b3fb6be59ed63c15d6dfad
NikitinSemen <pr.pypblrka@gmail.com>
2024-04-01 19:08:59 +0400

    написаны команды

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,7 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE table_name - команда для создания таблицы с требуемыми колонками
+(
+name_column type
+name_column type
+)
+INSERT INTO table_name (%s, %s) -   команда для ввода значений в выше перечисленные колонки
